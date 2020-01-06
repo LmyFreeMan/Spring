6d2e64b9f18975acd1b1b1ade02b543996410f81
Coder <2515946609@qq.com>
2020-01-06 19:33:44 +0800

    Spring的生命周期-后置处理器

--- src/com/spring/cycle/MyBeanPostProcessor.java
+++ src/com/spring/cycle/MyBeanPostProcessor.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
+    /*
+
+   public Object postProcessBeforeInitialization(Object bean, String beanName) init-method方法之前被调用
+   public Object postProcessAfterInitialization(Object bean, String beanName)  init-method方法之后被调用
+   bean:bean实例本身
+   beanName:IOC容器配置的bean的名字
+   */
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("22");
         System.out.println("postProcessBeforeInitialization:"+beanName);
