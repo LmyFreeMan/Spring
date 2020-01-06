9a864d2ea2bef2143ab9543c81cb84a124bbc204
Coder <2515946609@qq.com>
2020-01-06 19:36:51 +0800

    Test-spring自动装配

--- src/com/spring/test/Test_AutoWire.java
+++ src/com/spring/test/Test_AutoWire.java
@@ -4,7 +4,7 @@ import com.spring.autowire.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test_AutoWire {
+public class Test_AutoWire  {
     public static void main(String[] args) {
         ApplicationContext ctx=new ClassPathXmlApplicationContext("autowire.xml");
         Person person= (Person) ctx.getBean("person");
