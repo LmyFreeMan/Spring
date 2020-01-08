1f704d6c7a1b59b1207446e7c6df3ac835187c1c
Coder <2515946609@qq.com>
2020-01-08 16:12:33 +0800

    BaseService

--- src/com/spring/generic/di/BaseService.java
+++ src/com/spring/generic/di/BaseService.java
@@ -3,8 +3,12 @@ package com.spring.generic.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+
+
 public class BaseService<T> {
-    protected BaseRepository<T> repository;
+
+    @Autowired
+    protected  BaseRepository repository;
     public void add(){
         System.out.println("add..");
         System.out.println(repository);
