7a45463309aa49b617615080b6ceb871859c5c04
Coder <2515946609@qq.com>
2020-01-08 16:11:15 +0800

    生命周期

--- src/com/spring/test/Test_Cycle.java
+++ src/com/spring/test/Test_Cycle.java
@@ -8,7 +8,8 @@ public class Test_Cycle {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("bean-cycle.xml");
         Car car= (Car) ctx.getBean("car");
-        System.out.println(car);
+
+        //System.out.println(car);
         // 关闭ioc容器
         ctx.close();
 
